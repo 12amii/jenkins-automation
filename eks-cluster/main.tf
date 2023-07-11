4e48790b046ed55ca9f936f45c87f042c15b4f69
12amii <138985295+12amii@users.noreply.github.com>
2023-07-11 23:21:42 +0530

    Update main.tf

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -7,48 +7,46 @@ module "jenkins-server-vpc" {
 
   azs            = data.aws_availability_zones.azs.names
   private_subnets = var.private_subnet_cidr_blocks
-    public_subnets  = var.public_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
   enable_nat_gateway = true
   enable_vpn_gateway = true
   enable_dns_hostnames = true
 
-  tags = {
+    tags = {
     "kubernetes.io/cluster/jenkins-server-eks-cluster" = "shared"
     }
-    public_subnets_tags =  {
-      "kubernetes.io/cluster/jenkins-server-eks-cluster" = "shared"
+    public_subnets_tags = {
+        "kubernetes.io/cluster/jenkins-server-eks-cluster" = "shared"
         "kubernetes.io/role/elb" = 1
         }
 
-  private_subnets_tags = {
-         "kubernetes.io/cluster/jenkins-server-eks-cluster" = "shared"
+    private_subnets_tags = {
+        "kubernetes.io/cluster/jenkins-server-eks-cluster" = "shared"
         "kubernetes.io/role/internal-elb" = 1
 }
 }
 
 
 module "eks" {
- source = "terraform-aws-modules/eks/aws"
-version = "~> 19.0"
-cluster_name    = "jenkins-server-eks-cluster"
-  cluster_version = "1.24"
-  cluster_endpoint_public_access  = true
-vpc_id = module.jenkins-server-vpc.vpc_id
-subnet_ids = module.jenkins-server-vpc.private_subnets
+    source = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0"
+    cluster_name    = "jenkins-server-eks-cluster"
+    cluster_version = "1.24"
+    cluster_endpoint_public_access  = true
+    vpc_id = module.jenkins-server-vpc.vpc_id
+    subnet_ids = module.jenkins-server-vpc.private_subnets
 
-tags = {
-environment = "deployment"
-application = "myjenkins-server"
-}
-
-
+    tags = {
+        environment = "deployment"
+        application = "myjenkins-server"
+    }
 
 eks_managed_node_groups = {
 dev = {
-     min_size     = 1
-      max_size     = 3
-      desired_size = 2
-      instance_type = ["t2.small"]
-}
-}
+        min_size     = 1
+        max_size     = 3
+        desired_size = 2
+        instance_type = ["t2.small"] 
+        }
+    }
 }
