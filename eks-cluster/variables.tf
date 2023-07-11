fb69d83911702f8cec9b219aa027c6c2c8d4f554
12amii <138985295+12amii@users.noreply.github.com>
2023-07-11 21:46:48 +0530

    Create variables.tf

---
+++ eks-cluster/variables.tf
@@ -0,0 +1,11 @@
+variable "private_subnets_cidr_blocks" {
+  type = list(string)
+}
+
+variable "public_sunets_cidr_blocks" {
+type = list(string)
+}
+
+variable "vpc_cidr_block" {
+    type = string
+}
